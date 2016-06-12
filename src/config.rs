d2ec2a09126b375ba07df92526c85b7c6cea23bb
Jan Schulte <hello@unexpected-co.de>
2016-06-12 20:49:18 +0200

    feat: add watch property to configuration file
    BREAKING CHANGE: The watch property is now mandatory and breaks
    existing configuration files

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,8 @@ use rustc_serialize::json::DecoderError;
 #[derive(RustcDecodable)]
 pub struct Config {
     pub language: String,
-    pub script: Vec<String>
+    pub script: Vec<String>,
+    pub watch: String
 }
 
 pub fn from_json(json: &str) -> Result<Config, DecoderError> {
@@ -22,10 +23,12 @@ mod tests {
               \"language\": \"rust\",
               \"script\": [
                 \"cargo test\"
-              ]
+              ],
+              \"watch\": \"src/\"
             }";
         let config = from_json(json).unwrap();
         assert_eq!(config.language, "rust");
         assert_eq!(config.script, vec!("cargo test"));
+        assert_eq!(config.watch, "src/");
     }
 }
